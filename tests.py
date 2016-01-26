691c10598cb0d02519edb1e8ece2fe30beda521d
pkadambi <pkadambi1@gmail.com>
2016-01-26 16:07:35 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -93,7 +93,7 @@ class Test_Assignment_01(unittest.TestCase):
         """
         i = 1
         self.assertTrue(type(i) == int)
-        self.assertTrue(isinstance(i, 1))  # These lines do the same type checking
+        self.assertTrue(isinstance(i, int))  # These lines do the same type checking
         i = float(i)
         self.assertTrue(isinstance(i, float))
         i = str(i)
